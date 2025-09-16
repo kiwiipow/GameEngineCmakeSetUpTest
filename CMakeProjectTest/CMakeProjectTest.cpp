3e4e699d2e40f5e0aec5f2397b169d43842aea5e
kiwiipow <paula2005nemo@gmail.com>
2025-09-16 23:24:12 +0200

    triying to install vcpkg and cmake
    added vcpk frim git
    added cmake from download
    created vs2022 project

---
+++ CMakeProjectTest/CMakeProjectTest.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProjectTest.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeProjectTest.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
